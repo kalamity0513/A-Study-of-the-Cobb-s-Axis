992949a80b5893d3d210849d9126cc8f4b13082e
-Kalyani Jijith- <115133535+kalamity0513@users.noreply.github.com>
2024-05-16 13:51:02 +1000

    The surgical pre-planning UI tool
    To address the problem of osteophytes, allow surgeons to interact with the method and be able to have autonomy in planning whilst visualising and to be able to draw upon their expertise

---
+++ UI/main_app.py
@@ -0,0 +1,8 @@
+#!/bin/python3.12
+
+from PointPlacementApp import PointPlacementApp
+
+if __name__ == "__main__":
+    # Example usage:
+    image_path = "/Users/kalyani/Desktop/AutoCobb/Data/IguanaIJ/IMG-0002-00500.dcm"
+    PointPlacementApp(image_path)
