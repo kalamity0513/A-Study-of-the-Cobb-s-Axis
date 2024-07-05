a940afab8d42e5aa42ba66ccd1aa448ce8a8b0d2
-Kalyani Jijith- <115133535+kalamity0513@users.noreply.github.com>
2024-07-05 17:18:21 +1000

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,20 @@
+#!/bin/python3.12
+
+from PointPlacementApp import PointPlacementApp
+
+if __name__ == "__main__":
+    # Example usage:
+    image_path = "/Users/kalyani/Desktop/AutoCobb/Data/FalconIJ/IMG-0001-00304.dcm"
+    dicom_folder = '/Users/kalyani/Desktop/AutoCobb/Data/ChimpIJ'
+    # PointPlacementApp(image_path)
+    PointPlacementApp(dicom_folder)
+
+
+#image_paths = [
+    #"/Users/kalyani/Desktop/AutoCobb/Data/FalconIJ/IMG-0001-00304.dcm", 
+   # "/Users/kalyani/Desktop/AutoCobb/Data/JaguarIJ/IMG-0003-00487.dcm", 
+   # "/Users/kalyani/Desktop/AutoCobb/Data/IguanaIJ/IMG-0002-00500.dcm", 
+   # "/Users/kalyani/Desktop/AutoCobb/Data/ArmadilloIJ/IMG-0002-00521.dcm", 
+   # "/Users/kalyani/Desktop/AutoCobb/Data/ChimpIJ/IMG-0003-00505.dcm", 
+   # "/Users/kalyani/Desktop/AutoCobb/Data/GorillaIJ/IMG-0002-00474.dcm"
+   # "/Users/kalyani/Desktop/AutoCobb/Data/KangarooIJ/IMG-0001-00518.dcm",]
